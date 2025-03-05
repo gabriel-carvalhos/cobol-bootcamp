d34cd439d07ff3f9394839616f23ea3854c0876f
gabriel-carvalhos <gabrielcarvalhos.snt@gmail.com>
2025-03-05 15:29:59 -0300

    aula 3 - semana 5

--- semana-05/ARQ002.cob
+++ semana-05/ARQ002.cob
@@ -1,8 +1,8 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. ARQ001.
+       PROGRAM-ID. ARQ002.
       *=================================================================
       *==  AUTOR: Gabriel           EMPRESA: XPTO
-      *==  OBJETIVO: ABERTURA DE ARQUIVO CLIENTES.DAT
+      *==  OBJETIVO: LEITURA DE 1 REGISTRO DO ARQUIVO CLIENTES.DAT
       *==  DATA: 05/03/2025
       *==  OBSERVACOES: 
       *=================================================================
@@ -26,32 +26,47 @@
        01  REG-CLIENTES.
            05  REG-ID           PIC 9(004).
            05  REG-NOME         PIC X(020).
-           05  REG-TELEFONE     PIC X(004).
+           05  REG-TELEFONE     PIC X(011).
 
        WORKING-STORAGE       SECTION.
-       77  FS-CLIENTES          PIC 9(002).
+       77  FS-CLIENTES          PIC 9(002) VALUE ZEROS.
+       77  WRK-MSG-ERRO         PIC X(030) VALUE SPACES.
 
        PROCEDURE             DIVISION.
        0001-PRINCIPAL        SECTION.
            PERFORM 0100-INICIALIZAR.
+           PERFORM 0200-PROCESSAR.
            PERFORM 0300-FINALIZAR.
+
            STOP RUN.
            
        0100-INICIALIZAR      SECTION.
       *=== ABRE O ARQUIVO NO MODO LEITURA
            OPEN INPUT CLIENTES.
       
-      *=== VERIFICA SE O ARQUIVO PODE SER ABERTO, E EXIBE UMA MENSAGEM
+      *=== VERIFICA SE O ARQUIVO NAO PODE SER ABERTO
            IF FS-CLIENTES EQUAL 35
-               DISPLAY "ARQUIVO NAO FOI ABERTO"
-           ELSE
-               DISPLAY "ARQUIVO OK"
-           END-IF
+               MOVE "ARQUIVO NAO FOI ABERTO" TO WRK-MSG-ERRO
+               PERFORM 9000-TRATA-ERRO
+           END-IF.
 
        0200-PROCESSAR        SECTION.
-           CONTINUE.
+      *=== LE UM REGISTRO DO ARQUIVO, E CASO ESTAVA VAZIO, ATRIBUI O
+      *=== VALOR 10 PARA FS-CLIENTES    
+           READ CLIENTES.
+           IF FS-CLIENTES EQUAL 10
+               DISPLAY "ARQUIVO VAZIO"
+           ELSE
+               DISPLAY "ID.......... " REG-ID
+               DISPLAY "NOME........ " REG-NOME
+               DISPLAY "TELEFONE.... " REG-TELEFONE
+           END-IF.
 
        0300-FINALIZAR        SECTION.
            DISPLAY "FIM DE PROGRAMA".
       *=== FECHA O ARQUIVO
            CLOSE CLIENTES.
+       
+       9000-TRATA-ERRO       SECTION.
+           DISPLAY WRK-MSG-ERRO.
+           GOBACK.
