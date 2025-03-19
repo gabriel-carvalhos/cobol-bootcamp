b94313d7cb7ebab4b48c90a12afbe6f80b5b4d89
gabriel-carvalhos <gabrielcarvalhos.snt@gmail.com>
2025-03-18 22:01:21 -0300

    alteracoes encontro

--- semana-06/ARQ004.cob
+++ semana-06/ARQ004.cob
@@ -1,76 +1,105 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. ARQ004.
       *=================================================================
-      *==  AUTOR: Gabriel           EMPRESA: XPTO
+      *==  AUTOR: Gabriel           
+      *==  EMPRESA: XPTO
       *==  OBJETIVO: GRAVAÇÃO DE REGISTRO DO ARQUIVO CLIENTES.DAT
       *==  DATA: 05/03/2025
-      *==  OBSERVACOES: 
+      *==  OBSERVACOES:
       *=================================================================
        ENVIRONMENT           DIVISION.
        CONFIGURATION         SECTION.
        SPECIAL-NAMES.
            DECIMAL-POINT IS COMMA.
        INPUT-OUTPUT          SECTION.
-
+ 
        FILE-CONTROL.
       *=== DEFINE QUAL ARQUIVO SERA ABERTO, PASSANDO SEU CAMINHO,
       *=== E ATRIBUI O STATUS DA ABERTURA EM FS-CLIENTES
-           SELECT CLIENTES ASSIGN 
-           TO "D:\cobol-bootcamp\dados\CLIENTES.DAT"
-               FILE STATUS IS FS-CLIENTES.
-
+           SELECT CLIENTES ASSIGN
+           TO "..\dados\CLIENTES.DAT"
+               FILE STATUS IS FS-CLIENTES
+               ORGANIZATION IS LINE SEQUENTIAL.
+ 
+           SELECT ENTRADA ASSIGN
+           TO "..\dados\ENTRADA.TXT"
+               FILE STATUS IS FS-ENTRADA
+               ORGANIZATION IS LINE SEQUENTIAL.       
+     
        DATA DIVISION.
       *=== DEFINE O LAYOUT DO ARQUIVO
        FILE                  SECTION.
+       FD  ENTRADA.           
+      *-------------------
+      *---- TAMANHO -> 35
+      *-------------------
+       01  REG-ENTRADA-CLIENTES.
+           05  REG-ENTRADA-ID           PIC 9(004).
+           05  REG-ENTRADA-NOME         PIC X(020).
+           05  REG-ENTRADA-TELEFONE     PIC X(011).
+       
        FD  CLIENTES.
-       01  REG-CLIENTES.
-           05  REG-ID           PIC 9(004).
-           05  REG-NOME         PIC X(020).
-           05  REG-TELEFONE     PIC X(011).
-
+      *-------------------
+      *---- TAMANHO -> 35
+      *-------------------
+       01  REG-CLIENTES         PIC X(35).
+ 
        WORKING-STORAGE       SECTION.
        77  FS-CLIENTES          PIC 9(002) VALUE ZEROS.
+       77  FS-ENTRADA           PIC 9(002) VALUE ZEROS.
        77  WRK-MSG-ERRO         PIC X(030) VALUE SPACES.
-
+ 
        PROCEDURE             DIVISION.
        0001-PRINCIPAL        SECTION.
            PERFORM 0100-INICIALIZAR.
-           PERFORM 0200-PROCESSAR.
+           PERFORM 0200-PROCESSAR UNTIL FS-ENTRADA EQUAL 10.
            PERFORM 0300-FINALIZAR.
-
+ 
            STOP RUN.
            
        0100-INICIALIZAR      SECTION.
+      *=== ABRE O ARQUIVO NO MODO LEITURA 
+           OPEN INPUT ENTRADA
       *=== ABRE O ARQUIVO NO MODO GRAVACAO, O 'OUTPUT' DEFINE QUE O
       *=== NOVO CONTEUDO SERA SUBSTITUIDO PELO ANTIGO
            OPEN OUTPUT CLIENTES.
-      
-      *=== VERIFICA SE O ARQUIVO NAO PODE SER ABERTO
-           IF FS-CLIENTES EQUAL 35
-               MOVE "ARQUIVO NAO FOI ABERTO" TO WRK-MSG-ERRO
-               PERFORM 9000-TRATA-ERRO
-           END-IF.
 
-       0200-PROCESSAR        SECTION.
-           MOVE 0001                   TO REG-ID.
-           MOVE 'PAULA               ' TO REG-NOME.
-           MOVE '11942345647'          TO REG-TELEFONE.
-           WRITE REG-CLIENTES.
-
-           MOVE 0002                   TO REG-ID.
-           MOVE 'SANTOS              ' TO REG-NOME.
-           MOVE '11942345647'          TO REG-TELEFONE.
-           WRITE REG-CLIENTES.
+           EVALUATE FS-ENTRADA
+             WHEN 30
+                 MOVE "CAMINHO NAO RESOLVIDO" TO WRK-MSG-ERRO
+                 PERFORM 9000-TRATA-ERRO
+             WHEN 35    
+                 MOVE "ARQUIVO NAO FOI ABERTO" TO WRK-MSG-ERRO
+                 PERFORM 9000-TRATA-ERRO
+             WHEN 42   
+                 MOVE "ARQUIVO CORROMPIDO    " TO WRK-MSG-ERRO
+                 PERFORM 9000-TRATA-ERRO
+           END-EVALUATE.
+      *----------  TESTAR VAZIO         
+           READ ENTRADA.
            
-           MOVE 0003                   TO REG-ID.
-           MOVE 'LAURA               ' TO REG-NOME.
-           MOVE '11942345647'          TO REG-TELEFONE.
-           WRITE REG-CLIENTES.
-
+       0200-PROCESSAR        SECTION.
+       
+           IF REG-ENTRADA-TELEFONE(01:02) EQUAL "11"
+               MOVE REG-ENTRADA-CLIENTES TO REG-CLIENTES
+               
+               DISPLAY "ID........ " REG-ENTRADA-ID
+               DISPLAY "NOME...... " REG-ENTRADA-NOME
+               DISPLAY "TELEFONE.. " REG-ENTRADA-TELEFONE
+               DISPLAY "----------"
+               WRITE REG-CLIENTES
+           END-IF.
+                
+           READ ENTRADA.
+           IF FS-ENTRADA NOT EQUAL 00
+               DISPLAY "ERRO DE LEITURA"
+           END-IF.
+ 
        0300-FINALIZAR        SECTION.
            DISPLAY "FIM DE PROGRAMA".
       *=== FECHA O ARQUIVO
-           CLOSE CLIENTES.
+           CLOSE CLIENTES
+                 ENTRADA.
        
        9000-TRATA-ERRO       SECTION.
            DISPLAY WRK-MSG-ERRO.
